6c44d58f8ea007e37ff3b0b5d6df1ac5ad365959
Nosa Edoimioya <61152388+nosaedoimioya@users.noreply.github.com>
2025-05-12 16:25:14 -0500

    Adding more iago files (1)

---
+++ calibration-files/parameters_5kg.m
@@ -0,0 +1,19 @@
+function [wn,zeta] = parameters_5kg(J1)
+load('base_damping_stiffness_params_02-Mar-2023.mat') 
+load('shoulder_damping_stiffness_params_02-Mar-2023.mat')
+
+wn     = [];
+zeta   = [];
+
+k       = k0_base+k1_base*J1;
+c       = (c0_base+c1_base*sqrt(J1)+c2_base*J1)*(1+c_tau_base*J1);
+wn(1)   = sqrt(k/J1);
+zeta(1) = c/(2*sqrt(k*J1)); % 2*J1*wn = cc -> Critical damping
+
+
+k       = k0_shoulder+k1_shoulder*J1;
+c       = (c0_shoulder+c1_shoulder*sqrt(J1)+c2_shoulder*J1)*(1+c_tau_shoulder*J1);
+wn(2)   = sqrt(k/J1);
+zeta(2) = c/(2*sqrt(k*J1)); % 2*sqrt(k*J1) = cc -> Critical damping
+
+end
